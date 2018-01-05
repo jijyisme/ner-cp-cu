b080463ca109817f82fd69de2f73d355e4d0f518
Jijy Thamjarat <jijy.chavisa@gmail.com>
2018-01-05 15:22:14 +0700

    be able to customize the model

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 from loader import prepare_train_data
 import numpy as np
+
+from keras.models import load_model
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from keras.layers import RepeatVector
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from keras.layers.wrappers import Bidirectional
@@ -12,36 +13,53 @@ from sklearn.preprocessing import LabelEncoder
 from keras.utils import np_utils
 
 class NameEntityRecognizer(object):
-	def __init__(self,max_num_words,WORD_VEC_SIZE,tag_list):
-		self.max_num_words = max_num_words
-		self.tag_list = tag_list
-		self.model = Sequential()
-		lstm = LSTM(64,input_shape=(max_num_words,WORD_VEC_SIZE),return_sequences=True)
-		self.model.add(Bidirectional(lstm,
-		            input_shape=(max_num_words, WORD_VEC_SIZE)))
-		self.model.add(Bidirectional(lstm,
-		            input_shape=(max_num_words, WORD_VEC_SIZE)))
-		self.model.add(Bidirectional(lstm,
-		            input_shape=(max_num_words, WORD_VEC_SIZE)))
-		self.model.add(Dense(len(tag_list), activation='softmax'))
+	def __init__(self,model_path=None, max_num_words=300, word_vec_length=100):
+		self.model_status = ''
+		if model_path is None:
+			self.mode = 'new_model'
+			self.initialize_model(max_num_words, word_vec_length)
+		else:
+			self.mode = 'existing_model'
+			self.model = load_model(model_path)
 
-		self.model.compile(optimizer='adam',
-		              loss='categorical_crossentropy',
-		              metrics=['accuracy'])
-		print(self.model.summary())
-		
-	def tag_decode(self, y_dummy):
-		y_pred = []	
-		for post in y_dummy:
-			temp = []
-			for i in range(len(post)):
-				temp.append(self.tag_list[np.argmax(y_dummy[0][i])])
-			y_pred.append(temp)
-		return y_pred
+	def initialize_model(self, max_num_words, word_vec_length):
+		self.model = Sequential()
+		lstm = LSTM(64,input_shape=(max_num_words,word_vec_length),return_sequences=True)
+
+		self.model.add(Bidirectional(lstm,
+		            input_shape=(max_num_words, word_vec_length)))
+
+	def add_lstm_layer(self,input_shape, bidirectional=True):
+		if(self.mode == 'existing_model'):
+			print('The loaded model cannot be changed')
+			return
+		else:
+			lstm = LSTM(64,input_shape=input_shape,return_sequences=True)
+
+			self.model.add(Bidirectional(lstm,
+			            input_shape=input_shape))
+
+	def add_dense_layer(self, output_length, activation='softmax'):
+		if(self.mode == 'existing_model'):
+			print('The loaded model cannot be changed')
+			return
+		else:
+			self.model.add(Dense(output_length, activation=activation))
+
+	def compile(self):
+		if(self.mode == 'existing_model'):
+			print('The loaded model cannot be changed')
+			return
+		else:
+			self.model.compile(optimizer='adam',
+			              loss='categorical_crossentropy',
+			              metrics=['accuracy'])
+			print(self.model.summary())
 
 	def train(self, x_train, y_train):
 		return self.model.fit(x_train, y_train, epochs=1, batch_size = 128)		
 
 	def predict(self, data):
-		y_dummy = self.model.predict(data)
-		return self.tag_decode(y_dummy)
+		return self.model.predict(data)
+
+	def evaluate(self, ):
